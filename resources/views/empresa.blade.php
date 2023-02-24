20c0e836ff422178810c4503b2ff19d986a3c379
Eliel Godoy <eliel.godoy@etc.sp.gov.br>
2023-02-24 17:38:35 -0300

    Adição bootstrap

--- resources/views/empresa.blade.php
+++ resources/views/empresa.blade.php
@@ -4,19 +4,44 @@
 
     <meta charset="UTF-8">
     <title>Empresa</title>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
 </head>
 <body>
 
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">ROTAS</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/produtos">Produtos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/contato">Contato</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link active" href="/empresa">Empresa</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+
     <h1>EMPRESA</h1>
 
-    <a href="/"><button type="button" class="btn btn-danger"><ion-icon name="home-outline"></ion-icon>Home</button></a><br>
-    <a href="/produtos"><button type="button" class="btn btn-primary"><ion-icon name="bag-handle-outline"></ion-icon>Produtos</button></a><br>
-    <a href="/contato"><button type="button" class="btn btn-secondary"><ion-icon name="call-outline"></ion-icon>Contato</button></a><br>
+    <a href="/"><button type="button" class="btn btn-danger"><ion-icon name="home-outline"></ion-icon>Home</button></a>
+    <a href="/produtos"><button type="button" class="btn btn-primary"><ion-icon name="bag-handle-outline"></ion-icon>Produtos</button></a>
+    <a href="/contato"><button type="button" class="btn btn-secondary"><ion-icon name="call-outline"></ion-icon>Contato</button></a>
     
 </body>
 </html>
