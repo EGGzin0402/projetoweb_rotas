2890c498c079ac1c68d198125ddb74426e73b0d3
Eliel Godoy <eliel.godoy@etc.sp.gov.br>
2023-02-24 17:19:46 -0300

    Primeira commit

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="UTF-8">
+    <title>Empresa</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</head>
+<body>
+
+    <h1>EMPRESA</h1>
+
+    <a href="/"><button type="button" class="btn btn-danger"><ion-icon name="home-outline"></ion-icon>Home</button></a><br>
+    <a href="/produtos"><button type="button" class="btn btn-primary"><ion-icon name="bag-handle-outline"></ion-icon>Produtos</button></a><br>
+    <a href="/contato"><button type="button" class="btn btn-secondary"><ion-icon name="call-outline"></ion-icon>Contato</button></a><br>
+    
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</head>
+<body>
+
+    <h1>HOME</h1>
+
+    <a href="/produtos"><button type="button" class="btn btn-primary"><ion-icon name="bag-handle-outline"></ion-icon>Produtos</button></a><br>
+    <a href="/contato"><button type="button" class="btn btn-secondary"><ion-icon name="call-outline"></ion-icon>Contato</button></a><br>
+    <a href="/empresa"><button type="button" class="btn btn-success"><ion-icon name="business-outline"></ion-icon>Empresa</button></a><br>
+    
+</body>
+</html>
